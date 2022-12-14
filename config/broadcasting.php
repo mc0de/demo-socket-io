7de16493dcda9234023bbb36392c892b8d9f34e4
David Lun <mysticcode@gmail.com>
2022-12-14 02:10:14 +0200

    Updates broadcasting configuration for soketi

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -31,23 +31,26 @@ return [
     'connections' => [
 
         'pusher' => [
-            'driver' => 'pusher',
-            'key' => env('PUSHER_APP_KEY'),
-            'secret' => env('PUSHER_APP_SECRET'),
-            'app_id' => env('PUSHER_APP_ID'),
+            'driver'  => 'pusher',
+            'key'     => env('PUSHER_APP_KEY', 'app-key'),
+            'secret'  => env('PUSHER_APP_SECRET', 'app-secret'),
+            'app_id'  => env('PUSHER_APP_ID', 'app-id'),
             'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => true,
+                'host'      => env('PUSHER_HOST', '127.0.0.1'),
+                'port'      => env('PUSHER_PORT', 6001),
+                'scheme'    => env('PUSHER_SCHEME', 'http'),
+                'encrypted' => true,
+                'useTLS'    => env('PUSHER_SCHEME') === 'https',
             ],
         ],
 
         'ably' => [
             'driver' => 'ably',
-            'key' => env('ABLY_KEY'),
+            'key'    => env('ABLY_KEY'),
         ],
 
         'redis' => [
-            'driver' => 'redis',
+            'driver'     => 'redis',
             'connection' => 'default',
         ],
 
